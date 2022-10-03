4d4982a5fb1297384b24bd68b2a25b5ce94211af
Zhe Lee <xu35k6jo6@gmail.com>
2022-10-03 20:21:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication0919/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.myapplication0919;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView tx;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tx = findViewById(R.id.textView2); // 文字
+        btn = findViewById(R.id.button);//  按鈕
+
+        btn.setOnClickListener(this);
+        tx.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch( view.getId()){
+            case R.id.textView2:
+                tx.setTextColor(Color.GREEN);
+                tx.setText("方法三文字");
+                break;
+            case R.id.button:
+                tx.setTextColor(Color.BLACK);
+                tx.setText("方法三按鈕");
+                break;
+        }
+    }
+
+//    private View.OnClickListener myclick = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            switch(view.getId()){
+//                case R.id.button:
+//                    tx.setTextColor(Color.RED);
+//                    tx.setText("方法二按鈕");
+//                    break;
+//                case R.id.textView2:
+//                    tx.setTextColor(Color.GREEN);
+//                    tx.setText("方法二文字");
+//                    break;
+//            }
+//        }
+//    };
+
+//    public void OnBtnClick(View view){
+//        tx.setTextColor(Color.RED);
+//        tx.setText("方法一按鈕");
+//    }
+//    public void OnTextClick(View view){
+//        tx.setTextColor(Color.GREEN);
+//        tx.setText("方法一文字");
+//    }
+
+}
---
+++ myapplication2/src/main/java/com/example/myapplication2/MainActivity23.java
@@ -0,0 +1,13 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity23 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main23);
+    }
+}
